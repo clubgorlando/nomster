b41e14bff624a8260c72601bf16b7379b944705b
clubgorlando <eric.gorland@gmail.com>
2019-07-24 23:50:12 +0000

    added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters is the minumum required" }
   validates :address, presence: true
   validates :description, presence: true
+
 end
